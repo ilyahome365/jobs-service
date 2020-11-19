c4064021a19f3c5ee97131c67ddcf5038ce4309c
Shlomo BenDavid <shlomo@home365.co>
2020-11-19 17:58:44 +0200

    late-fee-job

--- src/main/java/com/home365/jobservice/controller/JobsController.java
+++ src/main/java/com/home365/jobservice/controller/JobsController.java
@@ -5,12 +5,14 @@ import com.home365.jobservice.service.impl.ApplicationServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
 @Slf4j
+@RestController
+@RequestMapping("/jobs")
 public class JobsController {
 
     private final ApplicationServiceImpl jobsService;
